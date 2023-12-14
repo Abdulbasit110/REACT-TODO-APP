fead6757ca265964c0a8043f33f1a4144f17759b
Abdulbasit110 <BASIT4408944@cloud.neduet.edu.pk>
2023-12-14 22:22:43 +0500

    react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let todo = document.getElementById("todoAdd").value;
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    };
+    if (newTodo.text.length > 0) {
+      setTodos([...todos].concat(newTodo));
+    } else {
+      alert("Enter Valid Task");
+    }
+    document.getElementById("todoAdd").value = "";
+  }
+
+  function deleteTodo(id) {
+    let updatedTodos = [...todos].filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  }
+
+  function toggleComplete(id) {
+    let updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="todo-list">
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" id="todoAdd" />
+        <button type="submit">Add Todo</button>
+      </form>
+      {todos.map((todo) => (
+        <div className="todo" key={todo.id}>
+          <div className="todo-text">{todo.text}</div>
+          <input
+            type="checkbox"
+            id="completed"
+            checked={todo.completed}
+            onChange={() => toggleComplete(todo.id)}
+          />
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
